83ae0a694c4b316fcb193acd6986d564787ec8e5
d.sin <d.sin@tinkoff.ru>
2023-11-09 02:09:46 +0300

    asdf

--- user-service.cpp
+++ user-service.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "user_request_handler.h"
-#include "../common/web_server/web_server.h"
-#include "../common/web_server/request_factory.h"
+#include "user-service/user_request_handler.h"
+#include "common/web_server/web_server.h"
+#include "common/web_server/request_factory.h"
 
 int main(int argc, char *argv[]) {
     auto *userRequestFactory = new WebServerRequestFactory<UserRequestHandler>({"/users", "search", "/auth"}, DateTimeFormat::SORTABLE_FORMAT);
