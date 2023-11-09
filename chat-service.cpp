3c73e7831fbc73e4c092b61c0275132f9ff0061b
d.sin <d.sin@tinkoff.ru>
2023-11-09 03:32:22 +0300

    asdf

--- chat-service.cpp
+++ chat-service.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "../common/web_server/web_server.h"
-#include "../common/web_server/request_factory.h"
-#include "message_request_handler.h"
+#include "common/web_server/web_server.h"
+#include "common/web_server/request_factory.h"
+#include "chat-service/message_request_handler.h"
 
 int main(int argc, char *argv[]) {
     auto *userRequestFactory = new WebServerRequestFactory<MessageRequestHandler>({"/chats"}, DateTimeFormat::SORTABLE_FORMAT);
--- post-service.cpp
+++ post-service.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include "../common/web_server/web_server.h"
-#include "../common/web_server/request_factory.h"
-#include "post_request_handler.h"
+#include "common/web_server/web_server.h"
+#include "common/web_server/request_factory.h"
+#include "post-service/post_request_handler.h"
 
 int main(int argc, char *argv[]) {
     auto *userRequestFactory = new WebServerRequestFactory<PostRequestHandler>({"/posts"}, DateTimeFormat::SORTABLE_FORMAT);
